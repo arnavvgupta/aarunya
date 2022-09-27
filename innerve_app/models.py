4db87ad873e5cc6d3b90ba5894c27eda9389adf0
Mansha Khattar <manshakhattar@gmail.com>
2022-09-27 13:16:00 +0530

    Adding backend code for patient and doctor

--- innerve_app/models.py
+++ innerve_app/models.py
@@ -1,3 +1,33 @@
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    # sno= models.AutoField(primary_key=True)
+     pname= models.CharField(max_length=255)
+     email= models.CharField(max_length=100)
+     dob= models.DateField(auto_now_add=True)
+     pf=models.TextField(null=True)
+     bg=models.CharField(max_length=13)
+     content= models.TextField()
+     extra= models.TextField()
+     password= models.TextField(null=True)
+
+     def __str__(self):                  # In Python, __str__ function is used to return the string representation of the object.
+         return "Message from " + self.pname + ' - ' + self.email 
+
+class Doctor(models.Model):
+    # sno= models.AutoField(primary_key=True)
+     dname= models.CharField(max_length=255)
+     email= models.CharField(max_length=100)
+     dob= models.DateField(null=False)
+     phone=models.IntegerField(null=True)
+     profile=models.CharField(max_length=23)
+     category= models.CharField(max_length=100)
+     qualification= models.CharField(max_length=100)
+     fee= models.CharField(max_length=100)
+    #  password1= models.TextField()
+    # timeStamp=models.DateTimeField(auto_now_add=True, blank=True)
+
+     def __str__(self):                  # In Python, __str__ function is used to return the string representation of the object.
+         return "Message from " + self.dname + ' - ' + self.email 
--- innerve_app/views.py
+++ innerve_app/views.py
@@ -1,46 +1,46 @@
-from django.shortcuts import render,HttpResponse
+from email import contentmanager
+from turtle import bgcolor
+from django.shortcuts import render,redirect,HttpResponse
+from .models import Doctor, Patient
+from django.contrib import messages
 
 def index(request):
     return render(request,'first.html')
 def choose(request):
     return render(request,'first.html')
 def patient(request):
-    return render(request,'patient.html')
-    # if request.method=="POST":
-    #     # Get the post parameters
-    #     username=request.POST['username']
-    #     email=request.POST['email']
-    #     fname=request.POST['fname']
-    #     lname=request.POST['lname']
-    #     pass1=request.POST['pass1']
-    #     pass2=request.POST['pass2']
-
-    #     # check for errorneous input
-    #     if len(username)>10:
-    #         messages.error(request, " Your user name must be less than 10 characters")
-    #         return redirect('home')
-
-    #     if not username.isalnum():
-    #         messages.error(request, " User name should only contain letters and numbers")
-    #         return redirect('home')
-    #     if (pass1!= pass2):
-    #          messages.error(request, " Passwords do not match")
-    #          return redirect('home')
+    #thank = False
+    if request.method=="POST":
         
-    #     # Create the user
-    #     myuser = User.objects.create_user(username, email, pass1)
-    #     myuser.first_name= fname
-    #     myuser.last_name= lname
-    #     myuser.save()
-    #     messages.success(request, " Your iCoder has been successfully created")
-    #     return redirect('home')
-
-    # else:
-    #     return HttpResponse("404 - Not found")
-
-
+        pname = request.POST['pname']
+        email = request.POST['email']
+        dob = request.POST['dob']
+        pf=request.POST['pf']
+        bg = request.POST['bg']
+        content = request.POST['content']
+        extra = request.POST['extra']
+        password = request.POST['password']
+        patient = Patient(pname=pname, email=email, dob=dob,pf=pf, bg=bg,content=content,extra=extra, password=password)
+        patient.save()
+        messages.success(request, " The patient has been registered!")
+        
+    return render(request,"patient.html")
 def doctor(request):
+    if request.method=="POST":
+        
+        dname = request.POST['dname']
+        email = request.POST['email']
+        dob = request.POST['dob']
+        phone=request.POST['phone']
+        profile = request.POST['profile']
+        category = request.POST['category']
+        qualification = request.POST['qualification']
+        fee= request.POST['fee']
+        doctor = Doctor(dname=dname, email=email, dob=dob, phone=phone,profile=profile,qualification=qualification,fee=fee,category=category)
+        doctor.save()
+        messages.success(request, " You have been registered!")
     return render(request,'doctor.html')
+
 def free(request):
     return render(request,'free.html')
 def paid(request):
