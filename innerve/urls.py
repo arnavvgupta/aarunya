147c2655951a8e97a88903c9ac9c7f28919bd887
Arnav Gupta <arnavgupta@Arnavs-MacBook-Air.local>
2022-09-30 20:03:35 +0530

    committ

--- innerve/urls.py
+++ innerve/urls.py
@@ -19,9 +19,4 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('innerve_app.urls')),
-<<<<<<< HEAD
-    path('blog/', include("blog.urls")),
-=======
-    path('blog/',include('blog.urls'))
->>>>>>> 0bf3128 (final)
 ]
